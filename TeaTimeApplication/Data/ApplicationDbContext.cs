71b9775041fb6e8f9468e9798acdd3a5c12bd710
Dino <woodming9889@gmail.com>
2025-03-11 22:48:44 +0800

    chore :  新增 override 的 OnModelCreating 屬性
    1.設定預設資料，方便更換開發環境時，新增 Migration 就能一起把預設的資料寫進資料庫

--- TeaTimeApplication/Data/ApplicationDbContext.cs
+++ TeaTimeApplication/Data/ApplicationDbContext.cs
@@ -14,5 +14,14 @@ namespace TeaTimeApplication.Data
         }
 
         public DbSet<CategoryModel> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryModel>().HasData(
+                new CategoryModel { Id = 1, Name = "茶飲", DisplayOrder = 1 },
+                new CategoryModel { Id = 2, Name = "水果茶", DisplayOrder = 2 },
+                new CategoryModel { Id = 3, Name = "咖啡", DisplayOrder = 3 }
+                );
+        }
     }
 }
