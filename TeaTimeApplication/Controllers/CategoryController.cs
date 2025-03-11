0dc42ce9d5c7b077871c2db2941703187b2b5ae7
Dino <woodming9889@gmail.com>
2025-03-11 23:15:40 +0800

    chore : 新增 CategoryController 的 Index

---
+++ TeaTimeApplication/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaTimeApplication.Controllers
+{
+    /// <summary>
+    /// 類別 Controller
+    /// </summary>
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
