f603d5425869aa52edd7c2cd31cd57db45c93b74
Dino <woodming9889@gmail.com>
2025-03-11 22:31:42 +0800

    chore : 新增 ApplicationDbContext.cs

---
+++ TeaTimeApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeaTimeApplication.Data
+{
+    /// <summary>
+    /// 資料庫 Context
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
