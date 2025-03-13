2b25f25ed5c9ff27453d4a02705853b9fd58dd53
Dino <woodming9889@gmail.com>
2025-03-13 22:17:09 +0800

    chore : 在 TeaTime.DataAccess 建立 Category 資料夾 及 新增 CategoryRepository 的 介面 & 實作

---
+++ TeaTime.DataAccess/Category/CategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using TeaTime.DataAccess.Data;
+using TeaTime.Models;
+
+namespace TeaTime.DataAccess.Category
+{
+    public class CategoryRepository : Repository<CategoryModel>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        /// <param name="db"></param>
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// 儲存 - 實作
+        /// </summary>
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        /// <summary>
+        /// 更新 實作
+        /// </summary>
+        /// <param name="category"></param>
+        public void Update(CategoryModel category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
---
+++ TeaTime.DataAccess/Category/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using TeaTime.Models;
+
+namespace TeaTime.DataAccess.Category
+{
+    public interface ICategoryRepository : IRepository<CategoryModel>
+    {
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="category"></param>
+        void Update(CategoryModel category);
+        
+        /// <summary>
+        /// 儲存
+        /// </summary>
+        void Save();
+    }
+}
