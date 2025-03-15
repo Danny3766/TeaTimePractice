b38911e5dec094d6a315f1cc4191adc4246dadd8
Dino <woodming9889@gmail.com>
2025-03-15 14:36:16 +0800

    chore : 新增 TeaTime.DataAccess / Product 資料夾並建立 ProductRepository 的介面及實作
    1.建立 ProductRepository 的介面: IProductRepository.cs
    2.建立 ProductRepository 的實作: ProductRepository.cs

---
+++ TeaTime.DataAccess/Product/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using TeaTime.DataAccess.Data;
+using TeaTime.Models;
+
+namespace TeaTime.DataAccess.Product
+{
+    public class ProductRepository : Repository<ProductModel>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        /// <param name="db"></param>
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// 儲存 - 實作
+        /// </summary>
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        /// <summary>
+        /// 更新 實作
+        /// </summary>
+        /// <param name="product"></param>
+        public void Update(ProductModel product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
