7efdae0703218c74bec66235d60f220a4b726668
Dino <woodming9889@gmail.com>
2025-03-15 13:38:59 +0800

    chore : 在 TeaTime.Models 新增 ProductModel
    1.替屬性加上 Attribute

---
+++ TeaTime.Models/ProductModel.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTime.Models;
+
+/// <summary>
+/// 產品 Model
+/// </summary>
+public class ProductModel
+{
+    /// <summary>
+    /// 產品 Id
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// 產品名稱
+    /// </summary>
+    [Required]
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// 產品 Size
+    /// </summary>
+    [Required]
+    public string Size { get; set; }
+    
+    /// <summary>
+    /// 產品價格
+    /// </summary>
+    [Required]
+    [Range(1, 10000)]
+    public double Price { get; set; }
+    
+    /// <summary>
+    /// 產品描述
+    /// </summary>
+    public string Descreption { get; set; }
+}
