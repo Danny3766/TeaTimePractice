db0d217b9954667eb11f71b45a84b565f52d04c1
Dino <woodming9889@gmail.com>
2026-06-27 18:07:16 +0800

    feat(cart): 新增購物車數量調整與移除功能
    - 新增 Plus action，可增加指定購物車項目的商品數量
    - 新增 Minus action，可減少商品數量，數量為 1 時移除項目
    - 新增 Remove action，可移除指定購物車項目
    - 補上購物車操作 action 的 XML 註解

--- TeaTimeApplication/Areas/Customer/Controllers/CartController.cs
+++ TeaTimeApplication/Areas/Customer/Controllers/CartController.cs
@@ -30,12 +30,70 @@ namespace TeaTimeApplication.Areas.Customer.Controllers
                 u.ApplicationUserId == userId, includeProperties: "Product")
             };
 
-            foreach(var cart in shoppingCartVM.ShoppingCartList)
+            foreach (var cart in shoppingCartVM.ShoppingCartList)
             {
                 shoppingCartVM.OrderTotal += (cart.Product.Price * cart.Count);
             }
 
             return View(shoppingCartVM);
+        }
+
+        /// <summary>
+        /// 增加指定購物車項目的商品數量。
+        /// </summary>
+        /// <param name="cartId">購物車項目 id。</param>
+        /// <returns>更新數量後重新導向至購物車頁面。</returns>
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// 減少指定購物車項目的商品數量，數量為 1 時移除該項目。
+        /// </summary>
+        /// <param name="cartId">購物車項目 id。</param>
+        /// <returns>更新數量或移除項目後重新導向至購物車頁面。</returns>
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+
+            if (cartFromDb.Count <= 1)
+            {
+                // 從購物車中移除該商品
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// 移除指定的購物車項目。
+        /// </summary>
+        /// <param name="cartId">購物車項目 id。</param>
+        /// <returns>移除項目後重新導向至購物車頁面。</returns>
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            
+            if (cartFromDb != null)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+                _unitOfWork.Save();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
